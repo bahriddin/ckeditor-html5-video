42934bc49d999c6c3fb1e07bd32656717e683a77
Mateus Tavares <mateus460@gmail.com>
2017-09-13 11:37:44 -0300

    Added translations into Portuguese

---
+++ html5video/lang/pt.js
@@ -0,0 +1,16 @@
+CKEDITOR.plugins.setLang( 'html5video', 'pt', {
+    button: 'Inserir vídeo HTML5',
+    title: 'HTML5 video',
+    infoLabel: 'Informações do Vídeo',
+    allowed: 'Extensões permitidas: MP4, WebM, Ogv',
+    urlMissing: 'O URL do vídeo está faltando.',
+    videoProperties: 'Propriedades do vídeo',
+    upload: 'Upload',
+    btnUpload: 'Enviar para o servidor',
+    advanced: 'Avançado',
+    autoplay: 'Autoplay?',
+    yes: 'Sim',
+    no: 'Não',
+    responsive: 'Largura responsiva',
+    controls: 'Exibir controles'
+} );
--- html5video/plugin.js
+++ html5video/plugin.js
@@ -1,6 +1,6 @@
 CKEDITOR.plugins.add( 'html5video', {
     requires: 'widget',
-    lang: 'de,en,eu,es,ru,uk,fr',
+    lang: 'de,en,eu,es,ru,uk,fr,pt,pt-br',
     icons: 'html5video',
     init: function( editor ) {
         editor.widgets.add( 'html5video', {
