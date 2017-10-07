83b7bf5a414073111904c9a8a840c8e805d14cc9
Unknown <darxide.neurofunk@gmail.com>
2017-10-07 15:14:20 +0200

    Create pl.js

---
+++ html5video/lang/pl.js
@@ -0,0 +1,16 @@
+CKEDITOR.plugins.setLang( 'html5video', 'pl', {
+    button: 'Wstaw HTML5 video',
+    title: 'HTML5 video',
+    infoLabel: 'Informacje',
+    allowed: 'Dozwolone typy plików: MP4, WebM, Ogv',
+    urlMissing: 'Nie znaleziono URL do pliku video.',
+    videoProperties: 'Właściwości',
+    upload: 'Wrzuć plik',
+    btnUpload: 'Wyślij na serwer',
+    advanced: 'Zaawansowane',
+    autoplay: 'Autoodtwarzanie?',
+    yes: 'Tak',
+    no: 'Nie',
+    responsive: 'Szerokość responsywna',
+    controls: 'Pokaż kontrolki'
+} );
--- html5video/plugin.js
+++ html5video/plugin.js
@@ -1,6 +1,6 @@
 CKEDITOR.plugins.add( 'html5video', {
     requires: 'widget',
-    lang: 'de,en,eu,es,ru,uk,fr',
+    lang: 'de,en,eu,es,ru,uk,fr,pl',
     icons: 'html5video',
     init: function( editor ) {
         editor.widgets.add( 'html5video', {
