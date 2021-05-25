a085a7fa5e2656754da561a96c1f74e05361c680
Mischa Sameli <m.sameli@backslash.ch>
2021-05-25 19:07:21 +0200

    added swiss german translation

--- html5video/plugin.js
+++ html5video/plugin.js
@@ -1,6 +1,6 @@
 CKEDITOR.plugins.add( 'html5video', {
     requires: 'widget',
-    lang: 'bg,ca,de,en,eu,es,ru,uk,fr,ko,pt,pt-br,pl',
+    lang: 'bg,ca,de,de-ch,en,eu,es,ru,uk,fr,ko,pt,pt-br,pl',
     icons: 'html5video',
     hidpi: true,
     init: function( editor ) {
